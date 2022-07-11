a9ba8f1f13148479b7b92902e7b7784674994698
evanlin917 <102419050+evanlin917@users.noreply.github.com>
2022-07-10 20:56:57 -0400

    Add files via upload

--- bitcoin.py
+++ bitcoin.py
@@ -6,3 +6,8 @@ import numpy_financial as npf
 bitcoin = [13657.20, 3843.52, 7200.17, 29374.15, 47686.81]
 current_std = np.std(bitcoin)
 print(current_std)
+
+#calculating IRR for 10 bitcoins mined from the year 2018-2022 assuming an initial investment of $500K
+current_irr_array = [-500000, 10 * bitcoin[0], 10 * bitcoin[1], 10 * bitcoin[2], 10 * bitcoin[3], 10 * bitcoin[4]]
+current_irr_res = npf.irr(current_irr_array)
+print(current_irr_res)
